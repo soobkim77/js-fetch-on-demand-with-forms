0e22c35e8d4a39edcc8e10fcfdd83e72cfa5b270
Soo-Beom <soobeomkim77@gmail.com>
2021-03-01 11:06:41 -0800

    commit tree daac49560cff8496c9edb121c328d3b2808ce016

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
-const init = () => {
-  
-}
+//Pass a function into an event listener that, when called, sends a fetch request then does something with the retreived data
 
+const init = () => {
+    const inputForm = document.querySelector('form') 
+    inputForm.addEventListener('submit', submitHandler)
+    function submitHandler(event){
+        event.preventDefault()
+        const input = document.querySelector('input#searchByID')
+        console.log(input)
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(response => response.json())
+        .then(data => {
+            const title = document.querySelector('section#movieDetails h4')
+            const summary = document.querySelector('section#movieDetails p')
+
+            title.innerText = data.title
+            summary.innerText = data.summary
+        })
+    }
+}
 document.addEventListener('DOMContentLoaded', init)
